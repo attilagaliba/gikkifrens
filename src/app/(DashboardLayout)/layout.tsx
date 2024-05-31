c8de5632f5a59cfc3fe3f1eb9790d15090340cce
attilagaliba <mehmetattilaozdemirps@gmail.com>
2024-05-31 13:25:38 +0300

    vercelAnalytics

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -8,6 +8,7 @@ import Sidebar from "@/app/(DashboardLayout)/layout/sidebar/Sidebar";
 import "@farcaster/auth-kit/styles.css";
 import { SignInButton, useProfile } from "@farcaster/auth-kit";
 import { checkUser } from "./func/galiba";
+import { Analytics } from "@vercel/analytics/react";
 import Link from "next/link";
 
 const MainWrapper = styled("div")(() => ({
@@ -110,6 +111,7 @@ export default function RootLayout({
 
   return (
     <MainWrapper className="mainwrapper">
+      <Analytics />
       {isAuthenticated && isInEarlyList && (
         <Sidebar
           isSidebarOpen={isSidebarOpen}
