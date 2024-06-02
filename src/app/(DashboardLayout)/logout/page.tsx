4fb5de2396c8386586fc2bf310d0ff79c4bf43bb
Mac <mac@Mac-MacBook-Pro.local>
2024-06-02 19:25:44 +0300

    logout

--- src/app/(DashboardLayout)/logout/page.tsx
+++ src/app/(DashboardLayout)/logout/page.tsx
@@ -1,30 +1,45 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Logout() {
   const router = useRouter();
+  const [loggingOut, setLoggingOut] = useState(false);
 
-  useEffect(() => {
-    // Clear localStorage
+  const handleLogout = () => {
+    setLoggingOut(true);
     localStorage.removeItem("isAuthenticated");
     localStorage.removeItem("userProfile");
-
-    // Redirect to login page
-    router.push("/");
-  }, [router]);
+    setTimeout(() => {
+      router.push("/");
+    }, 1000); // 1 saniye sonra yönlendirme yap
+  };
 
   return (
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
       }}
     >
-      <p>Logging out...</p>
+      <button
+        style={{
+          backgroundColor: "#6A5ACD",
+          color: "white",
+          padding: "10px 20px",
+          borderRadius: "5px",
+          cursor: "pointer",
+          border: "none",
+          outline: "none",
+        }}
+        onClick={handleLogout}
+      >
+        {loggingOut ? "Logging out..." : "Logout"}
+      </button>
     </div>
   );
 }
